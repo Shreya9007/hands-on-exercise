dfebba17ff115fe9490b3044e06745b80b71ac85
Shreya Sharma <shreyasharma0975@gmail.com>
2026-07-03 00:57:13 +0530

    Reorganize repository into Java and React folders

---
+++ exc/FactoryMethodPatternExample/ExcelFactory.java
@@ -0,0 +1,8 @@
+package FactoryMethodPatternExample;
+
+public class ExcelFactory extends DocumentFactory {
+    public Document createDocument(){
+        return new ExcelDocument();
+    }
+    
+}
---
+++ exc/FactoryMethodPatternExample/WordDocument.java
@@ -0,0 +1,10 @@
+package FactoryMethodPatternExample;
+
+class WordDocument implements Document{
+    public void open(){
+        System.out.println("Word Document Opened");
+    }
+    
+}
+
+
